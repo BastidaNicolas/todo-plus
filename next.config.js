356bd2b1eaef8f5b037c24a071ff448ea9d58c39
Nic <74965310+BastidaNicolas@users.noreply.github.com>
2023-09-04 20:04:22 -0300

    code cleanup

--- next.config.js
+++ next.config.js
@@ -2,38 +2,13 @@
 
 const withPWA = require("@ducanh2912/next-pwa").default({
     dest: "public",
-//   buildExcludes: ["app-build-manifest.json"],
     fallbacks: {
-        // Failed page requests fallback to this.
         document: "/offline",
     },
   });
 
-// const generateAppDirEntry = (entry) => {
-//     const packagePath = require.resolve('@ducanh2912/next-pwa');
-//     const packageDirectory = path.dirname(packagePath);
-//     const registerJs = path.join(packageDirectory, "register.js");
-  
-//     return entry().then((entries) => {
-//       if (entries["main-app"] && !entries["main-app"].includes(registerJs)) {
-//         if (Array.isArray(entries["main-app"])) {
-//           entries["main-app"].unshift(registerJs);
-//         } else if (typeof entries["main-app"] === "string") {
-//           entries["main-app"] = [registerJs, entries["main-app"]];
-//         }
-//       }
-//       return entries;
-//     });
-//   };
-
   const nextConfig = {
-    // reactStrictMode: true,
-    // webpack: (config) => {
-    //   const entry = generateAppDirEntry(config.entry);
-    //   config.entry = () => entry;
-  
-    //   return config;
-    // },
+    reactStrictMode: true,
   };
 
 module.exports = withPWA(nextConfig);
