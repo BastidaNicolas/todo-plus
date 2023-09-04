874fce4d1d872c6af73ee3f2998ef54922305014
Nic <74965310+BastidaNicolas@users.noreply.github.com>
2023-09-04 19:36:05 -0300

    build error fixed

--- next.config.js
+++ next.config.js
@@ -1,40 +1,38 @@
-/** @type {import('next').NextConfig} */
-const path = require("path");
+// const path = require("path");
 
-
-const withPWA = require("next-pwa")({
-  dest: "public",
-  buildExcludes: ["app-build-manifest.json"],
+const withPWA = require("@ducanh2912/next-pwa").default({
+    dest: "public",
+//   buildExcludes: ["app-build-manifest.json"],
 //   fallbacks: {
 //     document: '/_offline',
 //   },
-});
+  });
 
-const generateAppDirEntry = (entry) => {
-    const packagePath = require.resolve('next-pwa');
-    const packageDirectory = path.dirname(packagePath);
-    const registerJs = path.join(packageDirectory, "register.js");
+// const generateAppDirEntry = (entry) => {
+//     const packagePath = require.resolve('@ducanh2912/next-pwa');
+//     const packageDirectory = path.dirname(packagePath);
+//     const registerJs = path.join(packageDirectory, "register.js");
   
-    return entry().then((entries) => {
-      if (entries["main-app"] && !entries["main-app"].includes(registerJs)) {
-        if (Array.isArray(entries["main-app"])) {
-          entries["main-app"].unshift(registerJs);
-        } else if (typeof entries["main-app"] === "string") {
-          entries["main-app"] = [registerJs, entries["main-app"]];
-        }
-      }
-      return entries;
-    });
-  };
+//     return entry().then((entries) => {
+//       if (entries["main-app"] && !entries["main-app"].includes(registerJs)) {
+//         if (Array.isArray(entries["main-app"])) {
+//           entries["main-app"].unshift(registerJs);
+//         } else if (typeof entries["main-app"] === "string") {
+//           entries["main-app"] = [registerJs, entries["main-app"]];
+//         }
+//       }
+//       return entries;
+//     });
+//   };
 
   const nextConfig = {
-    reactStrictMode: true,
-    webpack: (config) => {
-      const entry = generateAppDirEntry(config.entry);
-      config.entry = () => entry;
+    // reactStrictMode: true,
+    // webpack: (config) => {
+    //   const entry = generateAppDirEntry(config.entry);
+    //   config.entry = () => entry;
   
-      return config;
-    },
+    //   return config;
+    // },
   };
 
 module.exports = withPWA(nextConfig);
