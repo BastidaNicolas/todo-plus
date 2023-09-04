a7939f2f27f554d45ce7eb2beb032cb6508d1fbc
Nic <74965310+BastidaNicolas@users.noreply.github.com>
2023-09-04 19:08:43 -0300

    sw should be working

--- next.config.js
+++ next.config.js
@@ -1,12 +1,40 @@
 /** @type {import('next').NextConfig} */
+const path = require("path");
+
 
 const withPWA = require("next-pwa")({
   dest: "public",
+  buildExcludes: ["app-build-manifest.json"],
 //   fallbacks: {
 //     document: '/_offline',
 //   },
 });
 
-const nextConfig = {};
+const generateAppDirEntry = (entry) => {
+    const packagePath = require.resolve('next-pwa');
+    const packageDirectory = path.dirname(packagePath);
+    const registerJs = path.join(packageDirectory, "register.js");
+  
+    return entry().then((entries) => {
+      if (entries["main-app"] && !entries["main-app"].includes(registerJs)) {
+        if (Array.isArray(entries["main-app"])) {
+          entries["main-app"].unshift(registerJs);
+        } else if (typeof entries["main-app"] === "string") {
+          entries["main-app"] = [registerJs, entries["main-app"]];
+        }
+      }
+      return entries;
+    });
+  };
+
+  const nextConfig = {
+    reactStrictMode: true,
+    webpack: (config) => {
+      const entry = generateAppDirEntry(config.entry);
+      config.entry = () => entry;
+  
+      return config;
+    },
+  };
 
 module.exports = withPWA(nextConfig);
