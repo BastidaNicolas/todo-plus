adac023c91b2fc77de86bac77138e012668a518b
Nic <74965310+BastidaNicolas@users.noreply.github.com>
2023-09-04 19:18:54 -0300

    the other page

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+    <main className="flex min-h-screen flex-col items-center justify-center">
       This should work
       <Link href={"other-page"}>Got to other page</Link>
     </main>
