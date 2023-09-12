162d408c0bcbad1a4c4801c774bc64c2b0eb232b
Nic <74965310+BastidaNicolas@users.noreply.github.com>
2023-09-12 19:45:21 -0300

    UI: home screen, created page template

--- app/offline/page.tsx
+++ app/offline/page.tsx
@@ -4,13 +4,13 @@ import GoBack from "@/components/buttons/goBackBtn";
 export default function Page() {
 
     return (
-      <div className="max-w-sm m-auto flex flex-col justify-center items-center min-h-screen">
+      <main className="max-w-sm m-auto flex flex-col justify-center items-center min-h-screen">
         <Logo className="w-16 h-16" />
-        <div className="py-10"></div>
+        <div className="pb-10"></div>
         <h1 className="font-bold text-base text-center">You are offline and this page isn’t cached</h1>
-        <div className="py-9"></div>
+        <div className="pb-8"></div>
         <GoBack/>
-      </div>
+      </main>
     );
   }
   
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,21 @@
+import Logo from '@/components/SVGs/logo';
+import { Button } from '@/components/ui/button';
+import { Mail } from 'lucide-react';
 import Image from 'next/image'
 import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      This should work
-      <Link href={"other-page"}>Got to other page</Link>
+    <main className="max-w-sm m-auto flex flex-col items-center justify-between h-screen">
+      <div className="flex flex-col items-center justify-end h-3/6">
+        <Logo className='h-16 w-16'/>
+        <div className='pb-28'></div>
+        <Button><Mail className="w-6 h-6 mr-2" />Login with Gmail</Button>
+      </div>
+      <div className='w-full text-center pb-4'>
+        <small className='text-sm font-medium leading-none'>Like the project?</small>
+        <a href='https://nicolas.bastida.dev/#contact' target='_blank' className="block text-lg font-semibold">Contact Developer</a>
+      </div>
     </main>
-  )
+  );
 }
