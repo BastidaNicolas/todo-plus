b0885668155922f3f246b2a2afdd2b0da9077475
Nic <74965310+BastidaNicolas@users.noreply.github.com>
2023-09-16 17:43:48 -0300

    UI: to-do state changes

--- app/to-do/[id]/page.tsx
+++ app/to-do/[id]/page.tsx
@@ -1,20 +1,37 @@
+"use client"
 import NavBtn from "@/components/buttons/navBtn";
 import StateBtn from "@/components/buttons/stateBtn";
+import { useState } from "react";
 
 export default function Page() {
+  const [tempState, setTempState] = useState("")
+
+    const handleFailure = () => {
+      setTempState("Failed");
+    }
+    const handleSuccess = () => {
+      setTempState("Completed")
+    }
+
     return (
       <main className="max-w-sm m-auto flex flex-col px-2 pt-4">
         <div className="flex flex-col gap-2">
           <h2 className="font-semibold text-3xl" >To-Do title</h2>
-          <small className="font-medium text-sm">14:00, May 23 2030</small>
+          <small className="font-medium text-sm">14:00, May 23 2030 <span className={`${tempState === "Failed" ? "text-red-600":"text-green-600"}`} >{tempState}</span></small>
           <p className="text-base whitespace-pre-line">
             {`The king, seeing how much happier his subjects were, realized the error of his ways and repealed the joke tax.\n\nPine apples.`}
           </p>
         </div>
-        <div className="flex justify-end pt-6 gap-4">
+        <div className={`flex justify-end pt-6 gap-4`}>
           <NavBtn variant="link" text="Edit" route="/to-do/1/edit"/>
-          <StateBtn text="I Failed :(" variant="destructive" />
-          <StateBtn text="Completed" variant="default" />
+          {!tempState ?
+            <>
+              <StateBtn onClick={handleFailure} text="I Failed :(" variant="destructive" />
+              <StateBtn onClick={handleSuccess} text="Completed" variant="default" />
+            </>
+            :
+            <></>
+          }
         </div>
       </main>
     );
--- components/buttons/stateBtn.tsx
+++ components/buttons/stateBtn.tsx
@@ -1,12 +1,9 @@
 "use client"
 import { Button } from "@/components/ui/button";
-import { useRouter } from 'next/navigation';
 
-export default function StateBtn({text, onClick, variant}:{text:string, onClick?:string, variant:"default"|"destructive"|"ghost"|"link"|"outline"|"secondary"}) {
-
-    const router = useRouter();
+export default function StateBtn({text, onClick, variant}:{text:string, onClick?:any, variant:"default"|"destructive"|"ghost"|"link"|"outline"|"secondary"}) {
 
     return (
-        <Button onClick={() => onClick} variant={variant} >{text}</Button>
+        <Button onClick={onClick} variant={variant} >{text}</Button>
     );
   }
