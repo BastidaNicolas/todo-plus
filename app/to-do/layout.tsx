e28fbe7708b87389514bfcd32289ae918e43c25f
Nic <74965310+BastidaNicolas@users.noreply.github.com>
2023-09-17 11:32:44 -0300

    SEO: metadata changed in /to-do

--- app/to-do/layout.tsx
+++ app/to-do/layout.tsx
@@ -1,14 +1,10 @@
 import Header from '@/components/header/Header'
 import type { Metadata } from 'next'
 
-// export const metadata: Metadata = {
-//   title: "To-Do Login",
-//   description: "This is the login page of To-Do",
-//   manifest: "/manifest/manifest.json",
-//   viewport: "width=device-width, initial-scale=1",
-//   icons: ["/favicon.ico"],
-//   themeColor: "#ffffff",
-// };
+export const metadata: Metadata = {
+  title: "To-Do",
+  description: "This is the application page of To-Do",
+};
 
 export default function ToDoLayout({
   children,
