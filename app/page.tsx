7fde43c1e01ccbae96226784162c91ddbd98ff59
Nic <74965310+BastidaNicolas@users.noreply.github.com>
2023-09-19 15:49:16 -0300

    little test

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   const session = useSession();
   const router = useRouter();
 
-  if(session.status === "authenticated"){
+  if(session){
     router.push("/to-do")
   }
 
